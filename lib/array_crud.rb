97045ef81cf2e6e5ced0b9469ed46277782bbc36
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-01-20 18:15:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["learn", "love", "code", "life"]
   
-  array << "fun"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
